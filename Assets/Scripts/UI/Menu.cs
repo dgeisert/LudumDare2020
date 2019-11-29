fce9fcad4a9290bc0dd0073cad06df3924bd673b
summitdavid <dg@summitstartups.com>
2019-11-29 07:35:48 -0800

    Initial Commit

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneChanger.LoadScene(Scenes.Game);
+    }
+    public void OpenSettings()
+    {
+
+    }
+    public void OpenControls()
+    {
+
+    }
+}
